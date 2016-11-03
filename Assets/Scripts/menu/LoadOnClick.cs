635b788a2c3537084a28256eb7803c7cec21849c
Robert Luckfield <rluck0419@gmail.com>
2016-11-03 15:41:39 -0500

    basic menu UI working
    scene loading does not fully load assets - missing materials - looking into varieties of loading (async and additive)

---
+++ Assets/Scripts/menu/LoadOnClick.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour {
+
+	public void LoadScene(int level) {
+		SceneManager.LoadSceneAsync (level);
+	}
+}
